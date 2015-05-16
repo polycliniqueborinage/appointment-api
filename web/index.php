4a1ce725b26247d2ccd2d5a000a4801b14912ff8
David Calcus <targoo@gmail.com>
2015-05-16 22:50:46 +0100

    add db and htaccess

--- web/index.php
+++ web/index.php
@@ -4,4 +4,22 @@ require_once '../vendor/autoload.php';
 // Init Silex app.
 $app = new Silex\Application();
 
+// define route for /countries
+$app->get('/countries', function () {
+  return "countries list";
+});
+
+// define route for /countries/{id}
+$app->get('/countries/{id}', function ($id) {
+  return "country's cities list for id: $id";
+// id must be digital
+})->assert('id', '\d+');
+
+// default route
+$app->get('/', function () {
+  return "List of avaiable methods:
+  - /countries - returns list of existing countries;
+  - /countries/{id} - returns list of country's cities by id;";
+});
+
 $app->run();
