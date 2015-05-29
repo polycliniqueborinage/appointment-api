a5a50c1820d7ad40c26c58d6b7275d74e8294697
David Calcus <targoo@gmail.com>
2015-05-29 17:59:42 +0100

    update speciality api to read the medcins table

--- src/PolyCliniqueBorinage/Services/SpecialityService.php
+++ src/PolyCliniqueBorinage/Services/SpecialityService.php
@@ -15,7 +15,7 @@ class SpecialityService extends BaseService {
    *  Return all specialities.
    */
   public function getAll() {
-    return $this->db->fetchAll("SELECT `id`, `value`, `label_fr`, `icon` FROM speciality WHERE `online_booking` = 1");
+    return $this->db->fetchAll("SELECT `id`, `specialite` as `value`, `icon` FROM `specialites` WHERE `online_booking` = 1");
   }
 
   /**
@@ -26,7 +26,7 @@ class SpecialityService extends BaseService {
    *  Return a speciality.
    */
   public function get($id) {
-    return $this->db->fetchAssoc("SELECT `id`, `value`, `label_fr`, `icon` FROM speciality WHERE id = :id AND `online_booking` = 1", array(
+    return $this->db->fetchAssoc("SELECT `id`, `specialite` as `value`, `icon` FROM `specialites` WHERE id = :id AND `online_booking` = 1", array(
       'id' => $id,
       )
     );
@@ -40,7 +40,7 @@ class SpecialityService extends BaseService {
    *  Return all doctors for the current speciality.
    */
   public function getDoctors($id) {
-    return $this->db->fetchAll("SELECT `user`.`id`, `user`.`firstname`, `user`.`familyname`, `user`.`gender`, `user`.`type`, `user`.`speciality`, `user`.`length_consult`, `user`.`inami` FROM `user` , `speciality` WHERE speciality.`value` = `user`.speciality AND speciality.id = :id", array(
+    return $this->db->fetchAll("SELECT `medecins`.`id`, `medecins`.`nom` as `lastname`, `medecins`.`prenom` as `firstname`, `medecins`.`length_consult` FROM `medecins` , `specialites` WHERE specialites.`id` = `medecins`.specialite AND `medecins`.`type` = 'interne' and `medecins`.`agenda` ='checked' AND `medecins`.`online_booking` = 1 AND specialites.`id` = :id", array(
         'id' => $id,
       )
     );
